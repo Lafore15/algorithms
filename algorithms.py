4ed51e01f791d239d847cdcf3a188832c64fc181
Lafore15 <mkov2n1517@gmail.com>
2023-07-04 12:19:27 +0300

    + bfs for binary tree

--- algorithms.py
+++ algorithms.py
@@ -12,3 +12,25 @@ def binary_search(values, target):
         else:
             right = mid - 1
     return -1
+
+
+#   bfs algorithms
+
+# for binary tree
+
+def bfs(root):
+    from collections import deque
+    deque = deque()
+    deque.append(root)
+    while deque:
+        cur_vertex = deque.popleft()
+        print(cur_vertex)
+        if cur_vertex.left:
+            deque.append(cur_vertex.left)
+        if cur_vertex.right:
+            deque.append(cur_vertex.right)
+    return True
+
+# for graphs
+
+
