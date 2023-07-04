68a71fd77dbc9ba4c1c6a10f869d414871efb078
Lafore15 <mkov2n1517@gmail.com>
2023-07-04 12:12:32 +0300

    +binary_search algorithm

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+# binary search
+def binary_search(values, target):
+    # values = sorted(values)
+    left = 0
+    right = len(values) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if values[mid] == target:
+            return mid
+        elif values[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
